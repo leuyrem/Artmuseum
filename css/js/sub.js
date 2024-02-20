aaa3091730e066a9834fc2d1d893496957b6ae76
leuyrem <leuyren@gmail.com>
2024-02-21 02:33:47 +0900

    18-1

--- css/js/sub.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    $('.picture').css("transform", "translateY(0px)").css("opacity", "1");
-    $('.box').css("transform", "translateY(0px)").css("opacity", "1");
-});
